0bde410ca12d11b6fa5eb05b55d2c4beb681d92b
victorhuang1 <geekle@yeah.net>
2021-06-07 06:48:48 -0600

    public folder for serving static files

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const http = require('http');
-const fs = require('fs')
-const homePage = fs.readFileSync('index.html')
+// const fs = require('fs')
+const path = require('path')
+
+/* const homePage = fs.readFileSync('index.html')
 const aboutPage = fs.readFileSync('about.html')
 const contactPage = fs.readFileSync('contact.html')
-const notFoundPage = fs.readFileSync('404.html')
-const server = http.createServer((req,res) => {
+const notFoundPage = fs.readFileSync('404.html') */
+
+const express = require('express')
+
+/* const server = http.createServer((req,res) => {
     if(req.url === '/about')
         res.end(aboutPage)
     else if(req.url === '/contact')
@@ -16,4 +21,21 @@ const server = http.createServer((req,res) => {
         res.end(notFoundPage)
     }
 });
-server.listen(3000)
+server.listen(3000) */
+
+const app = new express();
+app.use(express.static('public'))
+app.listen(3000,()=>{
+    console.log("App listening on port 3000")
+})
+
+app.get('/',(req,res)=>{
+    /* res.json({
+        name:"test"
+    }) */
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+})
+
+app.get('/about',(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'pages/about.html'))
+})
