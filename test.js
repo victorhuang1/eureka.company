496145caac81a9f0ecc9b302573067005a787dc0
victorhuang1 <geekle@yeah.net>
2021-06-18 07:43:03 -0600

    test on MongoDB

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const BlogPost = require('./models/BlogPost')
 mongoose.connect('mongodb://localhost/my_database',{useNewUrlParser:true});
 BlogPost.create({
-    title:'test',
-    body:'test1'
+    title:'Bruce Harris (journalist)',
+    body:'Life and career :Bruce Harris was born in Ireland and trained as a journalist in England in Middlesbrough and Birmingham before joining the Evening Standard in London in 1920 and turning his attention to sport.'
 },(error,blogpost)=>{
-    console.log(err,blogpost)
+    console.log(error,blogpost)
 })
